533e51bf4874e6e54df06d85a3407ca1781ed2c6
Elliot Marsden <elliot.marsden@habito.com>
2020-06-29 16:09:34 +0200

    Get stub interaction with API working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [decisionToken, setDecisionToken] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoaded(false)
+      setError(null)
+      // try {
+        const res = await fetch("/case/bar")
+        console.log(res)
+        const resJson = await res.json()
+        console.log(resJson)
+        console.log(resJson.decisionToken)
+        setDecisionToken(resJson.decisionToken)
+      // } catch (error) {
+      //   setError(error)
+      // }
+      setIsLoaded(true);
+    }
+    fetchData()
+  }, [])
+
+  let msg
+  if (error) {
+      msg = <p>Error: {error.message}</p>;
+  } else if (!isLoaded) {
+    msg = <p>Loading...</p>;
+  } else if (decisionToken) {
+    msg = <p>Decision token: {decisionToken}</p>
+  } else {
+    msg = <p>No decision token found</p>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
+        {msg}
         <a
           className="App-link"
           href="https://reactjs.org"
