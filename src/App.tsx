7ab7541d741e0ce0a60dcc2844ef2eec4915e086
Elliot Marsden <elliot.marsden@habito.com>
2020-07-02 16:22:57 +0200

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,108 @@
-import React from 'react'
-import {makeStyles, useTheme, Theme, createStyles, CssBaseline, AppBar, Toolbar, IconButton, Typography, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Hidden, Container, Grid, Paper, Table, TableHead, TableRow, TableCell, TableBody, FormControl, InputLabel, Select, MenuItem, CircularProgress} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
-import FolderIcon from '@material-ui/icons/Folder'
-import AssignmentIcon from '@material-ui/icons/Assignment';
+import React from "react";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Hidden,
+  Container,
+  Grid,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  CircularProgress,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import FolderIcon from "@material-ui/icons/Folder";
+import AssignmentIcon from "@material-ui/icons/Assignment";
 
 // API.
 
-type DecisionToken = string
+type DecisionToken = string;
 
 interface CaseSummary {
-  nextDecisionToken: DecisionToken
-  label: string
-  nrVariants: number
-  decisions: Array<DecisionSummary>
+  nextDecisionToken: DecisionToken;
+  label: string;
+  nrVariants: number;
+  decisions: Array<DecisionSummary>;
 }
 
 interface DecisionSummary {
-  token: DecisionToken
-  decisionTimeUTC: string
-  variant: number
+  token: DecisionToken;
+  decisionTimeUTC: string;
+  variant: number;
 }
 
 // /API.
 
-const drawerWidth = 240
+const drawerWidth = 240;
 
-type Page = 'cases' | 'decisions'
+type Page = "cases" | "decisions";
 
 interface PageData {
-  page: Page
-  label: string
-  icon: JSX.Element
+  page: Page;
+  label: string;
+  icon: JSX.Element;
 }
 
 const pageData: Array<PageData> = [
   {
-    page: 'cases',
-    label: 'Cases',
+    page: "cases",
+    label: "Cases",
     icon: <FolderIcon />,
   },
   {
-    page: 'decisions',
-    label: 'Decisions',
+    page: "decisions",
+    label: "Decisions",
     icon: <AssignmentIcon />,
   },
-]
+];
 
 const Title: React.FunctionComponent<{}> = (props) => (
   <Typography component="h2" variant="h6" color="primary" gutterBottom>
     {props.children}
   </Typography>
-)
+);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
+      display: "flex",
     },
     drawer: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: drawerWidth,
         flexShrink: 0,
       },
     },
     appBar: {
-      [theme.breakpoints.up('sm')]: {
+      [theme.breakpoints.up("sm")]: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
       },
     },
     menuButton: {
       marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none',
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
       },
     },
     // Put content below app bar
@@ -90,27 +120,27 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     contentPaper: {
       padding: theme.spacing(2),
-      display: 'flex',
-      overflow: 'auto',
-      flexDirection: 'column',
+      display: "flex",
+      overflow: "auto",
+      flexDirection: "column",
     },
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
     },
-  }),
-)
+  })
+);
 
 const App: React.FunctionComponent<{}> = (props) => {
-  const [mobileDrawerOpen, setMobileDrawerOpen] = React.useState(false)
-  const [currentPage, setCurrentPage] = React.useState<Page>('cases')
+  const [mobileDrawerOpen, setMobileDrawerOpen] = React.useState(false);
+  const [currentPage, setCurrentPage] = React.useState<Page>("cases");
 
   const handleMobileDrawerToggle = () => {
-    setMobileDrawerOpen(!mobileDrawerOpen)
-  }
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
 
-  const classes = useStyles()
-  const theme = useTheme()
+  const classes = useStyles();
+  const theme = useTheme();
 
   const appBar = (
     <AppBar position="fixed" className={classes.appBar}>
@@ -129,14 +159,14 @@ const App: React.FunctionComponent<{}> = (props) => {
         </Typography>
       </Toolbar>
     </AppBar>
-  )
+  );
 
   const drawer = (
     <div>
       <div className={classes.toolbar} />
       <Divider />
       <List>
-        {pageData.map(({page, label, icon}) => (
+        {pageData.map(({ page, label, icon }) => (
           <ListItem
             button
             key={page}
@@ -150,14 +180,14 @@ const App: React.FunctionComponent<{}> = (props) => {
       </List>
       <Divider />
     </div>
-  )
+  );
 
   const navDrawer = (
     <nav className={classes.drawer} aria-label="pages">
       <Hidden smUp implementation="css">
         <Drawer
           variant="temporary"
-          anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+          anchor={theme.direction === "rtl" ? "right" : "left"}
           open={mobileDrawerOpen}
           onClose={handleMobileDrawerToggle}
           classes={{
@@ -182,13 +212,13 @@ const App: React.FunctionComponent<{}> = (props) => {
         </Drawer>
       </Hidden>
     </nav>
-  )
+  );
 
-  let page
-  if (currentPage === 'cases') {
-    page = <Cases />
-  } else if (currentPage === 'decisions') {
-    page = <Decisions />
+  let page;
+  if (currentPage === "cases") {
+    page = <Cases />;
+  } else if (currentPage === "decisions") {
+    page = <Decisions />;
   }
 
   return (
@@ -201,40 +231,38 @@ const App: React.FunctionComponent<{}> = (props) => {
         {page}
       </main>
     </div>
-  )
-}
+  );
+};
 
 const Cases: React.FunctionComponent<{}> = (props) => {
-  const [error, setError] = React.useState<Error | null>(null)
-  const [caseSummaries, setCaseSummaries] = React.useState<Array<CaseSummary> | null>(null)
+  const [error, setError] = React.useState<Error | null>(null);
+  const [caseSummaries, setCaseSummaries] = React.useState<Array<
+    CaseSummary
+  > | null>(null);
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   React.useEffect(() => {
     const fetchData = async () => {
       if (caseSummaries == null) {
-        setError(null)
+        setError(null);
         try {
-          const res = await fetch("/case")
-          const resJson = await res.json()
-          setCaseSummaries(resJson)
+          const res = await fetch("/case");
+          const resJson = await res.json();
+          setCaseSummaries(resJson);
         } catch (error) {
-          setError(error)
+          setError(error);
         }
       }
-    }
-    fetchData()
-  }, [caseSummaries])
+    };
+    fetchData();
+  }, [caseSummaries]);
 
-  let content
+  let content;
   if (error) {
-    content = (
-      <p>Error: {error.message}</p>
-    )
+    content = <p>Error: {error.message}</p>;
   } else if (caseSummaries == null) {
-    content = (
-      <CircularProgress />
-    )
+    content = <CircularProgress />;
   } else {
     content = (
       <Table size="small">
@@ -249,21 +277,26 @@ const Cases: React.FunctionComponent<{}> = (props) => {
         </TableHead>
         <TableBody>
           {caseSummaries.map((caseSummary) => {
-            const decisions = caseSummary.decisions
-            const nrDecisions = decisions.length
-            const lastDecision = decisions[nrDecisions - 1]
+            const decisions = caseSummary.decisions;
+            const nrDecisions = decisions.length;
+            const lastDecision = decisions[nrDecisions - 1];
             return (
               <TableRow key={caseSummary.label}>
                 <TableCell>{caseSummary.label}</TableCell>
                 <TableCell>{caseSummary.nrVariants}</TableCell>
                 <TableCell>{nrDecisions}</TableCell>
-                <TableCell>{lastDecision ? lastDecision.decisionTimeUTC : ''}</TableCell>
-                <TableCell>{lastDecision ? lastDecision.variant : ''}</TableCell>
+                <TableCell>
+                  {lastDecision ? lastDecision.decisionTimeUTC : ""}
+                </TableCell>
+                <TableCell>
+                  {lastDecision ? lastDecision.variant : ""}
+                </TableCell>
               </TableRow>
-            )})}
+            );
+          })}
         </TableBody>
       </Table>
-    )
+    );
   }
 
   return (
@@ -277,66 +310,73 @@ const Cases: React.FunctionComponent<{}> = (props) => {
         </Grid>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
 const Decisions: React.FunctionComponent<{}> = (props) => {
-  const [caseLabelsError, setCaseLabelsError] = React.useState<Error | null>(null)
-  const [caseLabels, setCaseLabels] = React.useState<Array<string> | null>(null)
+  const [caseLabelsError, setCaseLabelsError] = React.useState<Error | null>(
+    null
+  );
+  const [caseLabels, setCaseLabels] = React.useState<Array<string> | null>(
+    null
+  );
 
-  const [caseSummaryError, setCaseSummaryError] = React.useState<Error | null>(null)
-  const [caseSummary, setCaseSummary] = React.useState<CaseSummary | null>(null)
+  const [caseSummaryError, setCaseSummaryError] = React.useState<Error | null>(
+    null
+  );
+  const [caseSummary, setCaseSummary] = React.useState<CaseSummary | null>(
+    null
+  );
 
-  const [selectedCaseLabel, setCaseLabel] = React.useState<string | null>(null)
+  const [selectedCaseLabel, setCaseLabel] = React.useState<string | null>(null);
 
-  const classes = useStyles()
+  const classes = useStyles();
 
   const handleSelectCase = (event: React.ChangeEvent<{ value: unknown }>) => {
     setCaseLabel(event.target.value as string);
   };
 
-
   React.useEffect(() => {
     const fetchData = async () => {
       if (caseLabels == null) {
         // Fetch labels.
-        setCaseLabelsError(null)
+        setCaseLabelsError(null);
         try {
-          const res = await fetch("/case")
-          const resJson = await res.json()
-          setCaseLabels(resJson.map((caseSummary: CaseSummary) => caseSummary.label))
+          const res = await fetch("/case");
+          const resJson = await res.json();
+          setCaseLabels(
+            resJson.map((caseSummary: CaseSummary) => caseSummary.label)
+          );
         } catch (error) {
-          setCaseLabelsError(error)
+          setCaseLabelsError(error);
         }
       } else if (selectedCaseLabel != null) {
         // Fetch summary for selected case.
-        setCaseSummary(null)
-        setCaseSummaryError(null)
+        setCaseSummary(null);
+        setCaseSummaryError(null);
 
         try {
-          const res = await fetch(`/case/${selectedCaseLabel}`)
-          const resJson = await res.json()
-          setCaseSummary(resJson)
+          const res = await fetch(`/case/${selectedCaseLabel}`);
+          const resJson = await res.json();
+          setCaseSummary(resJson);
         } catch (error) {
-          setCaseSummaryError(error)
+          setCaseSummaryError(error);
         }
       } else {
-        setCaseSummary(null)
-        setCaseSummaryError(null)
+        setCaseSummary(null);
+        setCaseSummaryError(null);
       }
-    }
-    fetchData()
-  }, [caseLabels, selectedCaseLabel])
+    };
+    fetchData();
+  }, [caseLabels, selectedCaseLabel]);
 
-  let caseSelectorElem
+  let caseSelectorElem;
   if (caseLabelsError) {
     caseSelectorElem = (
       <p>Error loading case labels: {caseLabelsError.message}</p>
-    )
+    );
   } else if (caseLabels == null) {
-    caseSelectorElem = (
-      <CircularProgress />
-    )
+    caseSelectorElem = <CircularProgress />;
   } else {
     caseSelectorElem = (
       <FormControl className={classes.formControl}>
@@ -347,52 +387,50 @@ const Decisions: React.FunctionComponent<{}> = (props) => {
           value={selectedCaseLabel}
           onChange={handleSelectCase}
         >
-          {caseLabels.map(caseLabel => (
+          {caseLabels.map((caseLabel) => (
             <MenuItem value={caseLabel}>{caseLabel}</MenuItem>
           ))}
         </Select>
       </FormControl>
-    )
+    );
   }
 
-  let caseSummaryElem
+  let caseSummaryElem;
   if (selectedCaseLabel == null) {
-    caseSummaryElem = null
+    caseSummaryElem = null;
   } else {
-    let summaryContent
+    let summaryContent;
     if (caseSummaryError) {
       summaryContent = (
         <p>Error loading case summary: {caseSummaryError.message}</p>
-      )
+      );
     } else if (caseSummary == null) {
-      summaryContent = (
-        <CircularProgress />
-      )
+      summaryContent = <CircularProgress />;
     } else {
       summaryContent = (
         <React.Fragment>
           <p>Number of variants: {caseSummary.nrVariants}</p>
 
-            <Table size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Decision time (UTC)</TableCell>
-                  <TableCell>Variant</TableCell>
-                  <TableCell>Token</TableCell>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Decision time (UTC)</TableCell>
+                <TableCell>Variant</TableCell>
+                <TableCell>Token</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {caseSummary.decisions.map((decisionSummary) => (
+                <TableRow key={decisionSummary.token}>
+                  <TableCell>{decisionSummary.decisionTimeUTC}</TableCell>
+                  <TableCell>{decisionSummary.variant}</TableCell>
+                  <TableCell>{decisionSummary.token}</TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {caseSummary.decisions.map(decisionSummary => (
-                  <TableRow key={decisionSummary.token}>
-                    <TableCell>{decisionSummary.decisionTimeUTC}</TableCell>
-                    <TableCell>{decisionSummary.variant}</TableCell>
-                    <TableCell>{decisionSummary.token}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+              ))}
+            </TableBody>
+          </Table>
         </React.Fragment>
-      )
+      );
     }
 
     caseSummaryElem = (
@@ -402,7 +440,7 @@ const Decisions: React.FunctionComponent<{}> = (props) => {
           {summaryContent}
         </Paper>
       </Grid>
-    )
+    );
   }
 
   return (
@@ -418,7 +456,7 @@ const Decisions: React.FunctionComponent<{}> = (props) => {
         {caseSummaryElem}
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
