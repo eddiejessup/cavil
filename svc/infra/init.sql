122edc9016034016366319a39781af792fd65977
Elliot Marsden <elliot.marsden@habito.com>
2020-07-23 16:35:41 +0100

    infra: Add SQL to initialise DB

---
+++ svc/infra/init.sql
@@ -0,0 +1,19 @@
+CREATE USER cavil WITH PASSWORD 'password123';
+
+CREATE DATABASE cavil;
+
+\connect cavil
+
+CREATE TYPE event_type AS ENUM ('case', 'ledger');
+
+CREATE TABLE event (
+    sequence_nr bigserial,
+    created timestamp with time zone not null default current_timestamp,
+    event_type event_type not null,
+    aggregate_id uuid not null,
+    data jsonb not null
+)
+;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO cavil;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO cavil;
